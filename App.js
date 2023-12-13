839b4812082a33b25397e8387d014322e7313bb9
ManVeraM <manuel.vera@alumnos.ucn.cl>
2023-12-13 15:03:39 -0300

    Added: api calls to front end

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/comments')
+      .then(response => {
+        setComments(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {posts.map((post, index) => (
+        <Text key={index}>{post.title}</Text>
+      ))}
+      {comments.map((comment, index) => (
+        <Text key={index}>{comment.body}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
